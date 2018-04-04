33b5e3b846a8aebf670295abca97c6c0f139ba3d
azzel <me@localhost.localdomain>
2018-04-04 06:27:50 +0500

    Macro shortcuts

--- src/logger.h
+++ src/logger.h
@@ -20,4 +20,26 @@ public:
   void setFormat(const QString &format);
 signals:
   void message(const QString &msg, const LogContext &, QtMsgType);
+private:
+  QtMsgType level_{QtInfoMsg};
 };
+
+#define logDebug(msg) Logger::instance().log(msg,__FILE__,__LINE__,QtDebugMsg)
+#define logInfo(msg)  Logger::instance().log(msg,__FILE__,__LINE__,QtInfoMsg)
+#define logWarn(msg)  Logger::instance().log(msg,__FILE__,__LINE__,QtWarningMsg)
+#define logFatal(msg) Logger::instance().log(msg,__FILE__,__LINE__,QtFatalMsg)
+
+#define LOGGER_NO_DEBUG_MACRO
+
+#if defined(LOGGER_NO_DEBUG_OUTPUT)
+#  undef logDebug
+#  define logDebug LOGGER_NO_DEBUG_MACRO
+#endif
+#if defined(LOGGER_NO_INFO_OUTPUT)
+#  undef logInfo
+#  define logInfo LOGGER_NO_DEBUG_MACRO
+#endif
+#if defined(LOGGER_NO_WARNING_OUTPUT)
+#  undef logWarning
+#  define logWarning LOGGER_NO_DEBUG_MACRO
+#endif
